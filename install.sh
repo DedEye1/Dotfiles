715490ad0f136e8e2245bdbfe81c27bd1da668eb
DedEye <dedeye103@gmail.com>
2026-09-03 20:48:15 +0600

    sync

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 chmod +x script/*
 ./script/pacman.sh
 ./script/yay.sh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 python script/dotfiles.py
 sudo systemctl enable sddm
-echo 'Setup is done. Reboot is recommended'
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
